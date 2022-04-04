75c2ad1e9735b7d63fe1582747280be47711e9c8
Bruce Atkinson <bruce@appinlet.com>
2022-04-04 04:12:25 +0200

    Initial release

---
+++ Nop.Plugin.Payments.Dpo/Models/ConfigurationModel.cs
@@ -0,0 +1,22 @@
+﻿using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
+
+namespace Nop.Plugin.Payments.Dpo.Models
+{
+    public record ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }      
+
+        [NopResourceDisplayName("Plugins.Payments.Dpo.Fields.CompanyToken")]
+        public string CompanyToken { get; set; }
+        public bool CompanyToken_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Dpo.Fields.ServiceType")]
+        public string ServiceType { get; set; }
+        public bool ServiceType_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.Payments.Dpo.Fields.UseSSL")]
+        public bool UseSSL { get; set; }
+        public bool UseSSL_OverrideForStore { get; set; }
+    }
+}
